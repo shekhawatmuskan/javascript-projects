e9b5ff6a713a74732c00f4f8d0d06d81d97d3f8e
shekhawatmuskan <muskanshekhawat73@gmail.com>
2026-01-30 23:12:50 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let saveEl = document.getElementById("save-el");
+let countEl = document.getElementById("count-el");
+
+let count = 0;
+
+function increment() {
+  count += 1;
+  countEl.innerText = count;
+}
+
+function save() {
+  console.log(count);
+}
