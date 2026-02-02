0b6f233918d36a0854793dc715d46ddde2c544fe
shekhawatmuskan <muskanshekhawat73@gmail.com>
2026-02-03 00:09:41 +0530

    made changes in blackjack

--- Blackjack-game/blackjack.js
+++ Blackjack-game/blackjack.js
@@ -2,8 +2,8 @@ let firstcard = 11;
 let secondcard = 11;
 let sum = firstcard + secondcard;
 let hasBlackjack = false;
-
 let isAlive = true;
+let message = "";
 
 // if (sum < 21) {
 //   console.log("Do you want to draw a new card?");
@@ -14,13 +14,13 @@ let isAlive = true;
 // }
 
 if (sum <= 20) {
-  console.log("Do you want to draw a new card?");
+  message = "Do you want to draw a new card?";
 } else if (sum === 21) {
-  console.log("Wohoo! You've got the Blackjack!");
+  message = "Wohoo! You've got the Blackjack!";
   hasBlackjack = true;
 } else {
-  console.log("You're out of the game!");
+  message = "You're out of the game!";
   isAlive = false;
 }
 
-console.log(hasBlackjack);
+console.log(message);
