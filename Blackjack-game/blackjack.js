a6a1b786c33757a03f6f831f17bd08eeae6294dc
shekhawatmuskan <muskanshekhawat73@gmail.com>
2026-02-04 22:13:24 +0530

    working till start btn

--- Blackjack-game/blackjack.js
+++ Blackjack-game/blackjack.js
@@ -1,26 +1,33 @@
-let firstcard = 11;
-let secondcard = 11;
-let sum = firstcard + secondcard;
-let hasBlackjack = false;
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-// if (sum < 21) {
-//   console.log("Do you want to draw a new card?");
-// } else if (sum === 21) {
-//   console.log("Wohoo! You've got the Blackjack!");
-// } else {
-//   console.log("You're out of the game!");
-// }
-
-if (sum <= 20) {
-  message = "Do you want to draw a new card?";
-} else if (sum === 21) {
-  message = "Wohoo! You've got the Blackjack!";
-  hasBlackjack = true;
-} else {
-  message = "You're out of the game!";
-  isAlive = false;
+function startGame() {
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  sumEl.textContent = "Sum: " + sum;
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
 }
 
-console.log(message);
+function newCard() {
+  console.log("Drawing a new card from the deck!");
+  // 1. Create a card variable, and hard code its value to a number (2-11)
+
+  // 2. Add the new card to the sum variable
+
+  // 3. Call startGame()
+}
